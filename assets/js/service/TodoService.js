aa08eed85b2c8d7b743eaf070813782daeaa2738
Angel Buil <angelpingel@gmail.com>
2016-05-06 00:16:41 +0200

    First user and permission config

--- assets/js/service/TodoService.js
+++ assets/js/service/TodoService.js
@@ -1,14 +1,17 @@
 todoApp.service('TodoService', function($http, $q) {
   return {
     'subscribeTodos':function(callback){
+      io.socket.get('/todo');
       io.socket.on('todo', function serverResponded (body, JWR){
         callback();
       });
     },
     'getTodos': function() {
       var defer = $q.defer();
-      io.socket.get('/todo', function serverResponded (body, JWR) {
-        defer.resolve(body);
+      $http.get('/todo').success(function(resp){
+        defer.resolve(resp);
+      }).error( function(err) {
+        defer.reject(err);
       });
       return defer.promise;
     },
